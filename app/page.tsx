8e06a00c55da959cdfc4f3a5d4b5b0f43db831f9
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2024-08-11 21:31:49 -0400

    feat: add shadcn css library

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,25 @@
 // import data from "./mockData.json";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
 import prisma from "./lib/prisma";
 
 export default async function Home() {
   const todos = await prisma.todo.findMany();
   return (
     <>
-      <main>
-        <div className="grid-cols-12 space-y-2">
-          {todos &&
-            todos.map((el) => {
-              return (
-                <div key={el.id}>
-                  <input
-                    type="checkbox"
-                    id={`${el.id}`}
-                    defaultChecked={el.checked}
-                  ></input>
-                  <label
-                    htmlFor={`${el.id}`}
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    {el.description}
-                  </label>
-                </div>
-              );
-            })}
-        </div>
+      <main className="container">
+        <ul className="grid gap-2">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex items-center space-x-2 py-5 px-4 bg-gray-100 rounded-md shadow"
+            >
+              <Checkbox id={`${todo.id}`} defaultChecked={todo.checked} />
+              <Input defaultValue={todo.description} />
+            </li>
+          ))}
+        </ul>
       </main>
     </>
   );
